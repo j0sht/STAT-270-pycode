752d70158e8fef90b90e9460f7c73a009c9c1b5e
Joshua Tate <jmtate@icloud.com>
2018-10-09 16:57:20 -0700

    Added binomial probability formula

--- formulas.py
+++ formulas.py
@@ -7,3 +7,11 @@ def npr(n,r):
 # Combination formula
 def ncr(n,r):
     return fact(n) // (fact(r)*fact(n-r))
+
+# Binomial Probability
+#  n = number of trials
+#  r = number of successes
+#  p = probability of success
+def binomial_probability(n,r,p):
+    # ncr(number of trials, successes) * p^successes * (1-p)^(trials-successes)
+    return ncr(n,r)*(p**r)*((1-p)**(n-r))
